8f94ec7d4138182009de732449116f6d45990712
Stevee04 <104826601+Stevee04@users.noreply.github.com>
2024-02-27 19:10:37 +0100

    examen

---
+++ scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: false,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+      nextArrow: '<button type="button" class="slick-next">Next</button>',
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  });
+  
